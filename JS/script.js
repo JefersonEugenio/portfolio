7ee50579fe59cf334ec03384a17162e65e271946
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-12-19 11:28:40 -0300

    Criar a pasta para JavaScript e configurar para responsividade

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger').addEventListener('click', function () {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('show'); // Mostra/esconde o menu
+});
