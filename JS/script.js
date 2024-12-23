d2ccb80f1ebcc8402194673438a9fc8169d8ce2f
Jeferson Eugenio <jefersonlopeseugenio11@gmail.com>
2024-12-23 09:45:55 -0300

    Modifiquei alguma coisa, modelo, css, js e adicionei imagem

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,33 @@ document.querySelector('.hamburger').addEventListener('click', function () {
     const menu = document.querySelector('.menu');
     menu.classList.toggle('show'); // Mostra/esconde o menu
 });
+
+const skills = [
+    { name: "JAVA", filled: 4, total: 5 },
+    { name: "PYTHON", filled: 2, total: 5 },
+    { name: "JAVASCRIPT", filled: 2, total: 5 },
+    { name: "SELENIUM", filled: 4, total: 5 },
+    { name: "REST ASSURED", filled: 3, total: 5 },
+    { name: "JUNIT 5", filled: 4, total: 5 },
+    { name: "HTML", filled: 3, total: 5 },
+    { name: "CSS", filled: 3, total: 5 },
+    { name: "PHP COM LARAVEL", filled: 2, total: 5 },
+    
+];
+
+const skillsList = document.getElementById('skills-list');
+
+    skills.forEach(skill => {
+        const li = document.createElement('li');
+        li.className = "skill-item";
+        li.innerHTML = `
+            <div>
+                <span class="font-bold">${skill.name}</span>
+                <div class="progress-bar" style="display: flex; gap: 4px; margin-top: 4px;">
+                    ${'<span class="filled" style="background: #eab53d; width: 20px; height: 20px; border-radius: 50%;"></span>'.repeat(skill.filled)}
+                    ${'<span class="empty" style="background: #ddd; width: 20px; height: 20px; border-radius: 50%;"></span>'.repeat(skill.total - skill.filled)}
+                </div>
+            </div>
+        `;
+        skillsList.appendChild(li);
+    });
